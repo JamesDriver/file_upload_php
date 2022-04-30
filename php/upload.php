3252ab54d8f37d88d11faca94557c5f9d04fffed
James Driver <jamesdriver1999@gmail.com>
2022-04-30 08:58:35 -0400

    vulnerable file sanitization
    file sanitization splits on the '.' in the file name, meaning that by uploading file.jpeg.php you can bypass the sanitization.

--- php/upload.php
+++ php/upload.php
@@ -2,8 +2,15 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = explode(".",$target_file)[1];
 
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
   echo "Sorry, your file is too large.";
